24c834b85b81b4d294a821716d51184b2844f122
Igor Pedrosa <ilap1036@gmail.com>
2021-12-21 13:58:50 -0300

    implementação do modal de comunicação

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import controllerUserLogin from './moduloInterface.js';
 
 var myModal = new bootstrap.Modal(document.getElementById('myModal'));
-debugger;
 
 const entrarUsuario = document.getElementById("btnLogin");
 entrarUsuario.addEventListener("click", (e)=>{
     let nome = document.getElementById("user").value,
     senha = document.getElementById("password").value;
-    if(!nome || nome === "" || !senha || senha === ""){
-        document.querySelector(".modal-body p").innerHTML = "Campos vazios";
-        myModal.show();
-    } else {
-        controllerUserLogin.loginInfo(nome, senha);
-    }
+    let logAttempt = controllerUserLogin.loginInfo(nome, senha);
+    modalResponse(logAttempt);
 });
 
+function modalResponse(msg){
+    document.querySelector(".modal-title").innerHTML = msg.title;
+    document.querySelector(".modal-body p").innerHTML = msg.bodyModal;
+    document.querySelector(".modal-content .b1").innerHTML = msg.b1;
+    document.querySelector(".modal-content .b2").innerHTML = msg.b2;
+    myModal.show();
+}
